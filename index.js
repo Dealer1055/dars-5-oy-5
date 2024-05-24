9036b43f3642d724af110d48e86ff8c2652eff10
Azizbek Nosirov <abb923609@gmail.com>
2024-05-24 11:54:43 +0500

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+let h1 = document.createElement("h1")
+
+h1.textContent = prompt("Text kiriting : ");
+
+let body = document.querySelector("body")
+
+body.appendChild(h1)
+
+body.style = "color: white; background-color: blue; text-align: center; padding: 10px 0";
+
+
+
+
+
+
+
